4807830502096e07947e07429c0007b70b1bce09
Yeji-Yoon <angelayoon0102@gmail.com>
2025-02-21 12:33:16 -0800

    s

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focus(){
+        console.log('Good!')
+    }
+    function blur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function change(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={change} />
+        </div>
     )
 }
 
